3e7e8a9f52248266c9e84323bf88a1e2b488ced1
Wilhelm <nightmare.v.94@gmail.com>
2018-04-27 12:52:01 +0500

    Шифровка, дешифровка, генерация бар кода

---
+++ Cypher/BarCodeGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Zen.Barcode;
+
+namespace Cypher
+{
+	public static class BarCodeGenerator
+	{
+		/// <summary>
+		/// Генерирует BarCode в формате Code128
+		/// </summary>
+		/// <param name="id">ID договора</param>
+		/// <param name="contractNumber">Номер договора</param>
+		/// <returns>BarCode в формате Base64</returns>
+		public static string GetBarcode(string id, string contractNumber)
+		{
+			const BarcodeSymbology type = BarcodeSymbology.Code128;
+			var drawObject = BarcodeDrawFactory.GetSymbology(type);
+			var metrics = drawObject.GetDefaultMetrics(60);
+			metrics.Scale = 2;
+
+			var endodedIdAndContractNumber = Encoder.BuildCode(id, contractNumber);
+			var barcodeImage = drawObject.Draw(endodedIdAndContractNumber, metrics);
+
+			using (var memoryStream = new MemoryStream())
+			{
+				barcodeImage.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
+				var imageBytes = memoryStream.ToArray();
+
+				return Convert.ToBase64String(imageBytes);
+			}
+		}
+	}
+}
---
+++ Cypher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Cypher
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			const string contractNumber = "Д-НС-09.11-15317/Т-НС-22.03-17316";
+			const string id = "12345";
+
+			var encrypted = Encoder.BuildCode(id, contractNumber);
+			var decrypted = Decoder.Reconstitute(encrypted);
+
+			if (decrypted.Key == id && decrypted.Value == contractNumber)
+			{
+				Console.WriteLine("It Works");
+			}
+
+			var base64BarCode = BarCodeGenerator.GetBarcode(id, contractNumber);
+		}
+	}
+}
